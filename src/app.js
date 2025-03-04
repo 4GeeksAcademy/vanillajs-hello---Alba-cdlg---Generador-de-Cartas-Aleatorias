8e4062f079e51ffdd0a7c2d24801c5dbc1107649
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-04 09:02:14 +0000

    boton y temporizador

--- src/app.js
+++ src/app.js
@@ -5,21 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
+  const cardElement = document.querySelector(".card");
+  const numberElement = document.querySelector(".number");
+  const button = document.querySelector("#newCardButton");
+
   let generateRandomNumber = () => {
     let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let indexNumber = Math.floor(Math.random() * number.length);
     return number[indexNumber];
   };
-  
+
   let generateRandomSuit = () => {
-    let card = ["spade", "clubs", "hearts", "diams"];
-    let indexCard = Math.floor(Math.random() * card.length);
-    return card[indexCard];
+    let suits = ["spade", "clubs", "hearts", "diams"];
+    let indexSuit = Math.floor(Math.random() * suits.length);
+    return suits[indexSuit];
   };
 
-  document.querySelector('.card').classList.add(generateRandomSuit());
-  document.querySelector('.number').innerHTML = generateRandomNumber();
+  let generateCard = () => {
+    cardElement.classList.remove("spade", "clubs", "hearts", "diams");
+    let newSuit = generateRandomSuit();
+    cardElement.classList.add(newSuit);
+    numberElement.innerHTML = generateRandomNumber();
+  };
 
+  generateCard();
+
+  button.addEventListener("click", generateCard);
+
+  setInterval(generateCard, 10000);
 };
