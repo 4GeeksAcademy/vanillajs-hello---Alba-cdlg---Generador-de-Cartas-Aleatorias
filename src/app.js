9ecd21b0249873d0f85401fc0acaeb7e961e2b70
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-04 08:30:14 +0000

    cambio automatico hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateRandomNumber = () => {
+    let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    return number[indexNumber];
+  };
+  
+  let generateRandomSuit = () => {
+    let card = ["spade", "clubs", "hearts", "diams"];
+    let indexCard = Math.floor(Math.random() * card.length);
+    return card[indexCard];
+  };
+
+  document.querySelector('.card').classList.add(generateRandomSuit());
+  document.querySelector('.number').innerHTML = generateRandomNumber();
+
 };
